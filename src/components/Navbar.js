42880dbcf64892e9a951090e741f5f482a149984
Chris Fagg <christopher.james.fagg@lego.com>
2022-03-03 13:27:02 +0000

    User update and delete with auth complete plus navbar buttons change on login and out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,65 @@
 //Navbar.js
 
 import { Link } from "react-router-dom";
+import { useEffect, useState, useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
+import { useNavigate} from 'react-router-dom';
 
 function Navbar() {
+  //check for login status via presence of token
+  const storedToken = localStorage.getItem("authToken");
+
+  const [ buttonToggle, setButtonToggle ] = useState(storedToken);
+
+  const { logOutUser } = useContext(AuthContext);
+  const navigate = useNavigate()
+
+  const buttonToggleSwitch = () => {
+    if (!storedToken) {
+      setButtonToggle(false);
+    } else {
+      setButtonToggle(true);
+    }
+  }
+
+  useEffect(()=> {
+    buttonToggleSwitch();
+  }, [storedToken]);
+
+
+  function handleLogout() {
+    logOutUser();
+
+    //navigate to landing page
+     navigate("/")
+  }
+
   return (
     <nav className="Navbar">
       ♢♧ Cardszilla ♡♤
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+      {!buttonToggle && 
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      }
 
-      <Link to="/signup">
-        <button>Signup</button>
-      </Link>
+      {!buttonToggle && 
+        <Link to="/signup">
+          <button>Signup</button>
+        </Link>
+      }
+
+      {buttonToggle &&
+        <Link to="/account">
+          <button>Account</button>
+        </Link>
+      }
+
+      {buttonToggle &&
+        <Link to="/">
+          <button onClick={handleLogout}>Logout</button>
+        </Link>
+      }
     </nav>
   );
 }
